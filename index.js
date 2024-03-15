6a8b8acd816d27263782de369337dd16ce29070e
Antonius025 <162035499+Antonius025@users.noreply.github.com>
2024-03-15 11:08:35 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const iniArray = ["McD", "Viking", "Intis"];
+
+console.log(iniArray[0]);
+
+// const arrayMatrix = [
+//   [1, 2, 3],
+
+//   [4, 5, 6],
+
+//   [7, 8, 9],
+// ];
+
+// arrayMatrix.push([0, 9, 8]);
+
+// arrayMatrix[0][1] = "X";
+
+// arrayMatrix[0][0] = "#";
+
+// for (let row of arrayMatrix) {
+//   const joinMatrix = row.join(" ");
+
+//   console.log(joinMatrix);
+// }
